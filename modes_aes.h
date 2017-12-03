ba274690b165e1cf167f5963bfad7f055ea8bf2b
Dani Maarouf <dani.maarouf@hotmail.com>
2017-12-03 03:29:03 -0500

    Update modes_aes.h

--- modes_aes.h
+++ modes_aes.h
@@ -20,7 +20,7 @@ void aes_ecb_256(const ap_uint<AES_W> in1[AES_N], const ap_uint<AES_W> in2[AES_N
 void aes_ctr_256(const ap_uint<AES_W> in[AES_N], ap_uint<AES_W> out[AES_N], const ap_uint<256> key, ap_uint<128> & ctr_init);
 
 #pragma SDS data access_pattern(in1:SEQUENTIAL, out1:SEQUENTIAL, in2:SEQUENTIAL, out2:SEQUENTIAL, in3:SEQUENTIAL, out3:SEQUENTIAL, in4:SEQUENTIAL, out4:SEQUENTIAL)
-#pragma SDS data mem_attribute(in1:PHYSICAL_CONTIGUOUS, out1:PHYSICAL_CONTIGUOUS, in2:PHYSICAL_CONTIGUOUS, out2:PHYSICAL_CONTIGUOUS, in3:PHYSICAL_CONTIGUOUS, out3:PHYSICAL_CONTIGUOUS, in4:PHYSICAL_CONTIGUOUS, out4:PHYSICAL_CONTIGUOUS)
+#pragma SDS data mem_attribute(in1:NON_CACHEABLE|PHYSICAL_CONTIGUOUS, out1:NON_CACHEABLE|PHYSICAL_CONTIGUOUS, in2:NON_CACHEABLE|PHYSICAL_CONTIGUOUS, out2:NON_CACHEABLE|PHYSICAL_CONTIGUOUS, in3:NON_CACHEABLE|PHYSICAL_CONTIGUOUS, out3:NON_CACHEABLE|PHYSICAL_CONTIGUOUS, in4:NON_CACHEABLE|PHYSICAL_CONTIGUOUS, out4:NON_CACHEABLE|PHYSICAL_CONTIGUOUS)
 #pragma SDS data sys_port(in1: AFI, out1:AFI, in2: AFI, out2:AFI, in3: AFI, out3:AFI, in4: AFI, out4:AFI)
 void aes_ctr_256_hp(const ap_uint<AES_W> in1[AES_N], const ap_uint<AES_W> in2[AES_N], const ap_uint<AES_W> in3[AES_N], const ap_uint<AES_W> in4[AES_N],
 		ap_uint<AES_W> out1[AES_N], ap_uint<AES_W> out2[AES_N], ap_uint<AES_W> out3[AES_N], ap_uint<AES_W> out4[AES_N], const ap_uint<256> key, ap_uint<128> & ctr_init);
